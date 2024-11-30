3df66de2e24922914b72365b21cbbbc69ce142e1
Benjamin Voisin <benjamin.voisin@ens-rennes.fr>
2024-11-30 20:12:46 +0100

    🐛🔀 put back pointers from merge

--- logic/tile.h
+++ logic/tile.h
@@ -9,12 +9,12 @@ typedef struct Tile Tile;
 
 Tile *tile_from_string(const char *);
 Tile *next_dora(const Tile *tile);
-int get_number(Tile t);
-bool is_adjacent(Tile t0, Tile t1);
-bool is_honor(Tile t);
-bool is_dragon(Tile t);
-bool is_wind(Tile t);
-bool is_family(Tile t);
+int get_number(Tile *t);
+bool is_adjacent(Tile *t0, Tile *t1);
+bool is_honor(Tile *t);
+bool is_dragon(Tile *t);
+bool is_wind(Tile *t);
+bool is_family(Tile *t);
 Texture2D tile_texture(const Tile *t);
 void free_tiles_textures();
 void pp_tile(FILE *file, Tile *t);
